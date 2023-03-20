dd3b4a4aac6ac6fa752afad719d7c27c277a172e
Jmussleh <Jmussleh@gmail.comgit config --global user.name Jmussleh Jmussleh@gmail.com>
2023-03-19 20:07:37 -0400

    Added questions to java and queryselectors

---
+++ assets/script.js
@@ -0,0 +1,36 @@
+var Start = document.querySelector("#startbtn");
+    var Quiz = document.querySelector("QuizContainer");
+    var Question = document.querySelector("#Question");
+    var Answers = document.querySelector("#Options");
+    var Timer = document.querySelector("#timer");
+
+let questions = [
+        {
+          question: "What does HTML stand for?",
+          answers: [
+            { text: "Hyper Text Markup Language", correct: true },
+            { text: "Hyperlinks Text Markup Language", correct: false },
+            { text: "Home Tool Markup Language", correct: false },
+            { text: "None of the above", correct: false }
+          ]
+        },
+        {
+          question: "What does CSS stand for?",
+          answers: [
+            { text: "Cascading Style Sheets", correct: true },
+            { text: "Computer Style Sheets", correct: false },
+            { text: "Colorful Style Sheets", correct: false },
+            { text: "None of the above", correct: false }
+          ]
+        },
+        {
+          question: "What is the correct syntax for referring to an external script called 'script.js'?",
+          answers: [
+            { text: "<script src='script.js'>", correct: true },
+            { text: "<script href='script.js'>", correct: false },
+            { text: "<script ref='script.js'>", correct: false },
+            { text: "<script name='script.js'>", correct: false }
+          ]
+        },
+      ]; 
+    
